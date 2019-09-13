87d31e91112dba9c2568697c89a90263711027ee
Murugesan Balasubramanian <Balasubramanian.Murugesan@liquidpay.com>
2019-09-13 11:55:04 +0800

    Initial Commit

---
+++ WeatherSPGroupApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeatherSPGroupApp
+//
+//  Created by Balasubramanian M on 12/9/19.
+//  Copyright © 2019 Balasubramanian M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
