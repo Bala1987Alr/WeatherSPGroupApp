744c4ec8bba30b71d6324ee9ebf1a77f3b03f188
Murugesan Balasubramanian <Balasubramanian.Murugesan@liquidpay.com>
2019-09-13 12:05:18 +0800

    Project created and pushed to GIT

---
+++ WeatherSPGroupApp/ViewModels/WeatherViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherViewModel.swift
+//  WeatherSPGroupApp
+//
+//  Created by Balasubramanian M on 13/9/19.
+//  Copyright © 2019 Balasubramanian M. All rights reserved.
+//
+
+import Foundation
